08f12e4dc3e0ef7750ee68c334df3d85887efcb3
fjlopez <fjlopez@unizar.es>
2021-07-27 17:02:18 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sitmun Backend Schema
+
+## Tools
+
+**GenerateSchema** is a database schema documentation generation tool for the project Sitmun.
+
+The output in [Asciidoc](https://asciidoctor.org/) serves for Sitmun 3 database documentation, 
+and it is designed to be diff-ed against other database schemas. 
+This is specially useful in Sitmun 3 because Sitmun will be deployed in different kind of database technologies.
+It is highly probable that each partner installation will have their own specific changes.
+
+Code is in an alpha state.
+The configuration of the current version is harcoded and expects a H2 file at `jdbc:h2:file:./build/sitmundb`.
+
+## Generated documentation
+
+The source of the generated documentation is in the folder [src/docs/asciidoc]. 
+In the folder [docs](docs) is the file [schema.pdf] that has been generated with `gradle asciidocPdf` from the source.
---
+++ src/main/java/org/sitmun/backend/schema/GenerateSchema.java
@@ -0,0 +1,78 @@
+package org.sitmun.backend.schema;
+
+import schemacrawler.inclusionrule.RegularExpressionInclusionRule;
+import schemacrawler.schemacrawler.*;
+import schemacrawler.tools.command.text.diagram.options.DiagramOutputFormat;
+import schemacrawler.tools.command.text.schema.options.SchemaTextOptionsBuilder;
+import schemacrawler.tools.command.text.schema.options.TextOutputFormat;
+import schemacrawler.tools.databaseconnector.DatabaseConnectionSource;
+import schemacrawler.tools.databaseconnector.SingleUseUserCredentials;
+import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+import schemacrawler.tools.options.Config;
+import schemacrawler.tools.options.OutputOptions;
+import schemacrawler.tools.options.OutputOptionsBuilder;
+import us.fatehi.utility.LoggingConfig;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.util.logging.Level;
+
+public final class GenerateSchema {
+
+    public static void main(final String[] args) throws Exception {
+
+        // Set log level
+        new LoggingConfig(Level.OFF);
+
+        // Create the options
+        final LimitOptionsBuilder limitOptionsBuilder = LimitOptionsBuilder.builder()
+            .includeSchemas(new RegularExpressionInclusionRule("SITMUNDB.PUBLIC"));
+        final LoadOptionsBuilder loadOptionsBuilder = LoadOptionsBuilder.builder()
+            .withSchemaInfoLevel(SchemaInfoLevelBuilder.maximum());
+        final SchemaCrawlerOptions options =
+                SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions()
+                        .withLimitOptions(limitOptionsBuilder.toOptions())
+                        .withLoadOptions(loadOptionsBuilder.toOptions());
+
+        try (Connection connection = getConnection()) {
+            createImage(options, getOutputFile("./src/docs/asciidoc/images/schema.svg"), connection);
+            createDocument(options, getOutputFile("./src/docs/asciidoc/schema.adoc"), connection);
+        }
+    }
+
+    private static void createDocument(SchemaCrawlerOptions options, Path file, Connection connection) throws Exception {
+        final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("template");
+        executable.setSchemaCrawlerOptions(options);
+        executable.setConnection(connection);
+        executable.setOutputOptions(OutputOptionsBuilder.newOutputOptions(TextOutputFormat.text, file));
+        final Config additionalConfig = new Config();
+        additionalConfig.put("template", "plaintextschema.vm");
+        additionalConfig.put("templating-language", "velocity");
+        executable.setAdditionalConfiguration(additionalConfig);
+        executable.execute();
+    }
+
+    private static void createImage(SchemaCrawlerOptions options, Path file, Connection connection) throws Exception {
+        final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("schema");
+        executable.setSchemaCrawlerOptions(options);
+        executable.setConnection(connection);
+        final OutputOptions output = OutputOptionsBuilder.newOutputOptions(DiagramOutputFormat.svg, file);
+        executable.setOutputOptions(output);
+        final Config additionalConfig = new Config();
+        additionalConfig.merge(SchemaTextOptionsBuilder.builder().portableNames().toConfig());
+        executable.setAdditionalConfiguration(additionalConfig);
+        executable.execute();
+    }
+
+    private static Connection getConnection() {
+        final String connectionUrl = "jdbc:h2:file:./build/sitmundb";
+        final DatabaseConnectionSource dataSource = new DatabaseConnectionSource(connectionUrl);
+        dataSource.setUserCredentials(new SingleUseUserCredentials("sample", ""));
+        return dataSource.get();
+    }
+
+    private static Path getOutputFile(final String name) {
+        return Paths.get(name).toAbsolutePath().normalize();
+    }
+}
